056ec44d093057171d352ce162618523dc6408eb
jacksonchaffey <jacksonchaffey@gmail.com>
2020-09-27 20:01:19 +1000

    added basic functionality of productivity planner and store of input, added text to options

--- time.rb
+++ time.rb
@@ -2,7 +2,7 @@
 
 class Login
 
-    attr_accessor :productiity_rating, :focus_tips, :logs
+    attr_accessor :productivity_rating, :focus_tips, :logs
     
     attr_reader :username
 
@@ -25,13 +25,14 @@ class Login
         choice = false
 
         while !choice
-            puts "Hello #{@username}\n"
-            puts "1.Productivity tracker"
+            puts "Hello #{@username}. Welcome to productively, please pick from the list below to begin.\n"
+            puts "1.Productivity tracker\n"
+            puts "2.Helpful hints\n"
             
             choice = gets.chomp.downcase
 
             if choice == "1" || choice == "productivity"
-                puts "Be productive"
+                productivity_plan
                 choice = true
             else
                 puts "Please choose an option from the list."
@@ -39,7 +40,39 @@ class Login
         end
     end
 
-end
+    def productivity_plan
+        
+        system("clear")
+    
+        time_frame = false
+
+        puts "How many hours of your day do you want to spend doing things you consider productive?"
+
+        while !time_frame
+            time = gets.chomp.to_i
+
+            if time >0 and time <16
+                valid_level = true
+                puts "Good luck today!"
+
+            elsif time>16
+                system("clear")
+                puts "\n\n We reccomend an average of 8 hours of sleep to ensure maximum productivity.\n Please reconsider how much time you want to spend being productive today."
+            else 
+                system("clear")
+                puts "Using numbers, please indicate how many hours of the day you want to spend being productive."
+            end
+        end
+
+        @productivity_rating.push(time)
+
+        if @productivity_rating.length == 1
+            main_menu
+        end
+
+    end
+        
+end 
 
 user = Login.new (@username)
 user.menu_screen

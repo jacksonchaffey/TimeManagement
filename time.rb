4ab11efabf24210cd9b1627a30fa6fa498b0fa1d
jacksonchaffey <jacksonchaffey@gmail.com>
2020-09-28 11:55:22 +1000

    more text prompts for choices, added frame for menu options and expanded on first option

--- time.rb
+++ time.rb
@@ -13,8 +13,7 @@ class Login
 
         @productivity_rating = []
 
-        @focus_tips =  [
-        ]
+        @focus_tips =  []
 
         @logs = []
 
@@ -25,15 +24,24 @@ class Login
         choice = false
 
         while !choice
+
             puts "Hello #{@username}. Welcome to productively, please pick from the list below to begin.\n"
             puts "1.Productivity tracker\n"
             puts "2.Helpful hints\n"
+            puts "3.Exit Program\n"
             
             choice = gets.chomp.downcase
 
             if choice == "1" || choice == "productivity"
                 productivity_plan
                 choice = true
+
+            elsif choice == "2" || choice == "help"
+                helpful_hints
+                choice = true
+            elsif choice == "3" || choice == "exit"
+                "Thanks for dropping by!"
+                exit
             else
                 puts "Please choose an option from the list."
             end
@@ -54,21 +62,30 @@ class Login
             if time >0 and time <16
                 valid_level = true
                 puts "Good luck today!"
+                sleep (1)
 
             elsif time>16
                 system("clear")
                 puts "\n\n We reccomend an average of 8 hours of sleep to ensure maximum productivity.\n Please reconsider how much time you want to spend being productive today."
+                sleep (2)
+                productivity_plan
             else 
                 system("clear")
                 puts "Using numbers, please indicate how many hours of the day you want to spend being productive."
             end
+
+            @productivity_rating.push(time)
+            
+            if @productivity_rating.length == 1
+                menu_screen
+
+            elsif @productivity_rating.length > 1
+                productivity_checker
+             end
         end
 
-        @productivity_rating.push(time)
 
-        if @productivity_rating.length == 1
-            main_menu
-        end
+        
 
     end
         

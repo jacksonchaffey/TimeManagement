268bfa2498d5b62103515e1c034956ae8a53a10b
jacksonchaffey <jacksonchaffey@gmail.com>
2020-10-04 13:42:09 +1100

    created frame for hints section

--- time.rb
+++ time.rb
@@ -43,18 +43,18 @@ class Login
             choice = gets.chomp.downcase
 
             if choice == "1" || choice == "productivity"
-                sleep_clear
+                system("clear")
                 productivity_plan
-                choice = true
+                
 
             elsif choice == "2" || choice == "help"
                 sleep_clear
                 helpful_hints
-                choice = true
+             
             elsif choice == "3" || choice == "diary"
                 sleep_clear
                 diary_log
-                choice = true    
+               
             elsif choice == "4" || choice == "exit"
                 "Thanks for dropping by!"
                 exit
@@ -89,8 +89,9 @@ class Login
 
                 elsif time>=16 and time <=24
                     system("clear")
-                    puts "\n\n We reccomend an average of 8 hours of sleep to ensure maximum productivity.\n Please reconsider how much time you want to spend being productive today."
-                    sleep (4)
+                    puts "We reccomend an average of 8 hours of sleep to ensure maximum productivity.\n Please reconsider how much time you want to spend being productive today."
+                    sleep (2)
+                    sleep_clear
                     productivity_plan
 
                 elsif time >24
@@ -109,13 +110,11 @@ class Login
             @productivity_rating.push(time)
             
 
-            ##
             if @productivity_rating.length == 1
                 menu_screen
 
             elsif @productivity_rating.length > 1
                 productivity_checker
-                menu_screen
              end
         end  
 
@@ -127,10 +126,10 @@ class Login
 
     def productivity_checker(productivity_rating)
 
-        if productivity_rating[1] > productivity_rating [0]
+        if (productivity_rating[1] > productivity_rating [0])
             puts "Not quite what you wanted to achieve today but thats ok!"
             random_hint
-        elsif time[-1] == time[-2]
+        elsif (productivity_rating[1] == productivity_rating[0])
             puts "Well Done!"
             menu_screen
         else 
@@ -140,26 +139,53 @@ class Login
     end
    
     # 
+    def helpful_hints
 
+           puts "Welcome to the helpful hints section where we provide tips to help you be productive!\n Would you like a 
+           single random tip or would you like to view the entire list?\n1.Random tip.\n2.View list.\n3.Back to main menu."
+
+           hints_choice = gets.chomp
+           
+            if (hints_choice == "1")
+                random_hint
+                sleep_clear
+                helpful_hints
+            elsif (hints_choice == "2")
+                puts focus_tips
+                sleep_clear
+                helpful_hints
+            elsif (hints_choice == "3")
+                sleep_clear
+                menu_screen
+            # puts focus_tips
+            # sleep(2)
+            # menu_screen
+            end
+
+
+    end
+    
+    
     def random_hint
 
         random = Random.new
 
-        puts "\n#{focus_tips[random.rand(1..@focus_tips.length)]}"
+        puts "\n#{random.focus_tips[random.rand(1..@focus_tips.length)]}"
         sleep_clear
         menu_screen
     end
 
     def diary_log
 
-        puts ("\nWelcome to your personal diary space. \nWould you like to view past dairies or write a new one?
-        \n 1. Write new entry  \n 2. View old entries \n 3. Back to main menu. ")
+        puts "\nWelcome to your personal diary space. \nWould you like to view past dairies or write a new one?
+        \n 1. Write new entry  \n 2. View old entries \n 3. Back to main menu. "
 
         diary_choice = gets.chomp
         
       
         
-        if (diary_choice =="1")
+        if (diary_choice =="1" || diary_choice == "new")
+            system("clear")
             puts("Begin your diary entry..\n")
             new_entry = gets.chomp()
             timestamp = Time.now
@@ -170,7 +196,7 @@ class Login
             puts "\n Entry successful."
             sleep_clear
 
-        elsif(diary_choice == "2")
+        elsif(diary_choice == "2" || diary_choice == "view")
                 if(logs == [])
                     system("clear")
                     puts "\n There are no previous entries."
@@ -205,29 +231,33 @@ def login_check
 
     profiles = 
     {
-    :jackson => "jackson123",
-    :admin => "admin123"
+    :Jackson => "jackson123",
+    :admin => "admin"
     }
 
-entry = false
-while (!entry)
-    puts ( "Hello! Please enter your username")
-        id = gets.chomp
-    puts ( "Please enter your password")
-        pw = gets.chomp
+    entry = false
+
+        while (!entry)
+         puts ( "Hello! Please enter your username")
+           id = gets.chomp
+         puts ( "Please enter your password")
+           pw = gets.chomp
     
-        if (id == "jackson" && profiles[:jackson] == pw)
-            entry = true
-        else
-            system ("clear")
-            puts ("\nInvalid login ID or password, please try again.")
+            if (id == "admin" && profiles[:admin] == pw)
+                entry = true
+
+           elsif (id == "Jackson" && profiles[:Jackson] == pw)
+               entry = true
+             
+           else
+                system ("clear")
+                puts ("\nInvalid login ID or password, please try again.")
+           end
+
         end
-    end
 
-@username = id
-system("clear")
-
-
+    @username = id
+    system("clear")
 end
 
 
